32e77f88f84bd16264d311d0de3b050ce20b6fae
YComputer <thea@163.com>
2016-10-11 12:57:02 +0800

    L-14

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import configureStore from './configureStore';
 import Root from './components/Root';
 
 const store = configureStore();
-
 render(
     <Root store={store}/>,
     document.getElementById('root')
